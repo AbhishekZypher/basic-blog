948417d3a564e54219a29ff9a71a826ad51d2039
Doctrexx <abhishek123488355@gmail.com>
2023-11-13 15:28:48 -0500

    used mongoose, find, sort, findbyid, displaying the data

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
+// mongodb connection string
+const dbURI = 'mongodb+srv://madarauchiha:mangekyo@cluster0.91jqczd.mongodb.net/basic-blog'
+mongoose.connect(dbURI)
+    // listen to requests only after the connection with database has been established
+    .then((result) => app.listen(3000))
+    .catch((error) => console.log(error))
+
 // register view engine 
 app.set('view engine', 'ejs');
-
-// listen for requests 
-app.listen(3000);
 
 // middleware for static files
 app.use(express.static('public'))
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-    const blogs = [
-        { title: 'fjasdhfjkadshfj', snippet: 'ashdfjksadlkkjca sbfsdffdshisao  iasufo ashfsdjd' },
-        { title: 'fjasdhfjkahdsjf', snippet: 'ashdfjksadlkkjca sbfsdffdshisao ashfsdjdfkadsjfh' },
-        { title: 'kadshfjkahdsjf', snippet: 'ashdfjksadlkkjca sbfsdffdshisao  iasufo ashadsjfh' }
-    ];
-    res.render('index', { blogs });
+    res.redirect('/blogs')
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
+
+// blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 }) //a method that we get on mongoose and -1 means the recent object that has been added will be displayed on the top, 'descending order '
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result })
+            console.log(result);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create Blog' });
